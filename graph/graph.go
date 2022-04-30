ba14319d29a531ac104df9095295b5d459b2f375
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2022-04-30 21:23:09 +0100

    Map notes to data graphs
    Create graphs of note links and tags to make it possible to gather
    indirect relationships between information (e.g. backlinks and
    adjacently related tags/topics).

---
+++ graph/graph.go
@@ -0,0 +1,34 @@
+package graph
+
+type Graph struct {
+	Vertices map[uint64]bool
+	Edges    map[uint64][]uint64
+}
+
+func New() *Graph {
+	return &Graph{
+		Vertices: map[uint64]bool{},
+		Edges:    map[uint64][]uint64{},
+	}
+}
+
+func (g *Graph) AddVertex(id uint64) {
+	g.Vertices[id] = true
+}
+
+func (g *Graph) AddEdge(v1, v2 uint64) {
+	if g.IsEdge(v1, v2) {
+		return
+	}
+	g.Edges[v1] = append(g.Edges[v1], v2)
+	g.Edges[v2] = append(g.Edges[v2], v1)
+}
+
+func (g *Graph) IsEdge(v1, v2 uint64) bool {
+	for _, v := range g.Edges[v1] {
+		if v == v2 {
+			return true
+		}
+	}
+	return false
+}
--- notebook/notebook.go
+++ notebook/notebook.go
@@ -1,27 +1,35 @@
 package notebook
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/msp301/zb/graph"
 	"github.com/msp301/zb/parser"
 )
 
 type Notebook struct {
-	Filters []FilterFunc
-	lookup  map[uint64]parser.Note
-	Path    string
-	Notes   []parser.Note
+	Filters   []FilterFunc
+	lookup    map[uint64]parser.Note
+	linkGraph *graph.Graph
+	tags      map[string]uint64
+	tagGraph  *graph.Graph
+	Path      string
+	Notes     []parser.Note
 }
 
 type FilterFunc func(note parser.Note) bool
 
 func New(path string) Notebook {
 	return Notebook{
-		Path:   path,
-		lookup: map[uint64]parser.Note{},
+		Path:      path,
+		lookup:    map[uint64]parser.Note{},
+		linkGraph: graph.New(),
+		tags:      map[string]uint64{},
+		tagGraph:  graph.New(),
 	}
 }
 
@@ -47,6 +55,25 @@ func (book *Notebook) Read() []parser.Note {
 	NOTE:
 		for _, note := range fileNotes {
 			book.lookup[note.Id] = note
+			book.linkGraph.AddVertex(note.Id)
+
+			for _, link := range note.Links {
+				book.linkGraph.AddEdge(note.Id, link)
+			}
+
+			for _, tag := range note.Tags {
+				tagId := book.addTag(tag)
+				book.tagGraph.AddVertex(tagId)
+			}
+
+			for _, tag := range note.Tags {
+				for _, relatedTag := range note.Tags {
+					if relatedTag == tag {
+						continue
+					}
+					book.tagGraph.AddEdge(book.tags[tag], book.tags[relatedTag])
+				}
+			}
 
 			for _, filter := range book.Filters {
 				if filter(note) {
@@ -59,10 +86,39 @@ func (book *Notebook) Read() []parser.Note {
 		return nil
 	})
 
+	tagKeys := map[uint64]string{}
+	for key, val := range book.tags {
+		tagKeys[val] = key
+	}
+
+	tagMap := map[string][]string{}
+	for tag, relatedTags := range book.tagGraph.Edges {
+		for _, relatedTag := range relatedTags {
+			tagStr, ok := tagKeys[tag]
+			if ok {
+				tagMap[tagStr] = append(tagMap[tagStr], tagKeys[relatedTag])
+			}
+		}
+	}
+
+	//fmt.Println(book.linkGraph.Edges)
+	//fmt.Println(book.tagGraph.Edges)
+	fmt.Println(book.tagGraph.Vertices)
+	fmt.Println(tagMap)
+
 	return filteredNotes
 }
 
 func (book *Notebook) GetNote(noteId uint64) (parser.Note, bool) {
 	note, ok := book.lookup[noteId]
 	return note, ok
+	//note, ok := book.lookup.GetVertex(noteId)
+	//return note, ok
+}
+
+func (book *Notebook) addTag(tag string) uint64 {
+	tagNum := len(book.tags)
+	tagId := uint64(tagNum + 1)
+	book.tags[tag] = tagId
+	return tagId
 }
