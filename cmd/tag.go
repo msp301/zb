4269da0d33cd6d3508b87470d777f6a72f974fa7
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2024-04-28 22:04:28 +0100

    Fix incompatibility with being given 'fzf' query strings
    'fzf' appears to pass a single quoted query string to "bind" commands
    which 'zb tag' will treat as a single tag name, but multiple tags won't
    match because tags don't contain spaces.
    
    If we're given a single argument that has spaces in it, break the string
    up by splitting on spaces and treating each term as an individual tag as
    this is more likely what the user meant.

--- cmd/tag.go
+++ cmd/tag.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"strings"
 )
 
 var tagCmd = &cobra.Command{
@@ -9,6 +10,9 @@ var tagCmd = &cobra.Command{
 	Short: "Find anything directly related to one or more tags",
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 1 {
+			args = strings.Fields(args[0])
+		}
 		renderResults(book().SearchByTags(args...))
 	},
 }
