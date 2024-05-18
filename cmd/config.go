4f267e4c678290605bf5c0d1fab2aa637a28acb4
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2024-05-18 19:33:31 +0100

    Allow writing local and global configuration
    Can write the entire current state of the config to the intended
    destination using 'config --global --save' but 'config --global' won't
    print out the global configuration because we've already loaded the
    configuration context based on where 'zb' has been called.
    
    It might be better to support the idea of global and local partial
    configurations that build the complete configuration allowing us to
    globally set our editor of choice but having notebook directories
    specific to the notebook folders.

--- cmd/config.go
+++ cmd/config.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/msp301/zb/config"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -13,8 +14,14 @@ var configCmd = &cobra.Command{
 	Short: "Get and set configuration options",
 	Run: func(cmd *cobra.Command, args []string) {
 		configFile := viper.ConfigFileUsed()
+		isGlobal, _ := cmd.Flags().GetBool("global")
+
 		if configFile != "" {
 			fmt.Printf("Config file used: %s\n\n", viper.ConfigFileUsed())
+		} else if isGlobal {
+			configFile = config.GlobalConfigFile
+		} else {
+			configFile = config.ConfigFile
 		}
 
 		jsonStr, _ := json.MarshalIndent(viper.AllSettings(), "", "  ")
@@ -22,14 +29,14 @@ var configCmd = &cobra.Command{
 
 		save, _ := cmd.Flags().GetBool("save")
 		if save {
-			viper.WriteConfig()
-			fmt.Println("Configuration saved to", viper.ConfigFileUsed())
+			viper.WriteConfigAs(configFile)
+			fmt.Println("Configuration saved to", configFile)
 		}
 	},
 }
 
 func init() {
+	configCmd.PersistentFlags().BoolP("global", "g", false, "Use global configuration file")
 	configCmd.PersistentFlags().BoolP("save", "s", false, "Write current configuration to file")
-	viper.BindPFlag("save", configCmd.PersistentFlags().Lookup("save"))
 	rootCmd.AddCommand(configCmd)
 }
