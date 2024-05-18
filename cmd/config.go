823da99be8902ca8f504c324171339ecfc69a917
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2024-05-18 21:03:59 +0100

    Edit configuration from command line
    * Get and set individual config options.
    * Array/Slice options accept multiple values to populate them
    * Add '--edit' option to open config up in editor to edit manually

--- cmd/config.go
+++ cmd/config.go
@@ -3,8 +3,10 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 
 	"github.com/msp301/zb/config"
+	"github.com/msp301/zb/editor"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -24,18 +26,52 @@ var configCmd = &cobra.Command{
 			configFile = config.ConfigFile
 		}
 
-		jsonStr, _ := json.MarshalIndent(viper.AllSettings(), "", "  ")
-		fmt.Println(string(jsonStr))
+		if edit, _ := cmd.Flags().GetBool("edit"); edit {
+			editor.Open(configFile)
+			return
+		}
 
-		save, _ := cmd.Flags().GetBool("save")
-		if save {
+		if len(args) == 0 {
+			jsonStr, _ := json.MarshalIndent(viper.AllSettings(), "", "  ")
+			fmt.Println(string(jsonStr))
+
+			save, _ := cmd.Flags().GetBool("save")
+			if save {
+				viper.WriteConfigAs(configFile)
+				fmt.Println("Configuration saved to", configFile)
+			}
+
+			return
+		}
+
+		option := args[0]
+		if !viper.InConfig(option) {
+			fmt.Println("Option not found")
+			return
+		}
+
+		if len(args) == 1 {
+			value := viper.Get(option)
+			fmt.Println(value)
+		} else {
+			value := viper.Get(option)
+			rt := reflect.TypeOf(value)
+			switch rt.Kind() {
+			case reflect.Array:
+			case reflect.Slice:
+				viper.Set(option, args[1:])
+			default:
+				viper.Set(option, args[1])
+			}
+
 			viper.WriteConfigAs(configFile)
-			fmt.Println("Configuration saved to", configFile)
+			fmt.Println("Configuration updated")
 		}
 	},
 }
 
 func init() {
+	configCmd.PersistentFlags().BoolP("edit", "e", false, "Open configuration file in editor")
 	configCmd.PersistentFlags().BoolP("global", "g", false, "Use global configuration file")
 	configCmd.PersistentFlags().BoolP("save", "s", false, "Write current configuration to file")
 	rootCmd.AddCommand(configCmd)
