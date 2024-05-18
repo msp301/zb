84c26895038c75ae4f71fe7e6994d1748442d6a3
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2024-05-18 14:47:01 +0100

    Output config file used when printing configuration

--- cmd/config.go
+++ cmd/config.go
@@ -12,6 +12,10 @@ var configCmd = &cobra.Command{
 	Use:   "config",
 	Short: "Get and set configuration options",
 	Run: func(cmd *cobra.Command, args []string) {
+		configFile := viper.ConfigFileUsed()
+		if configFile != "" {
+			fmt.Printf("Config file used: %s\n\n", viper.ConfigFileUsed())
+		}
 		jsonStr, _ := json.MarshalIndent(viper.AllSettings(), "", "  ")
 		fmt.Println(string(jsonStr))
 	},
