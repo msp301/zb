e0c025c9f815295c33a37f92c7de6a3c9ef2fef2
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2024-05-24 22:31:09 +0100

    Refactor pager functionality into a package
    Make it possible to output to the pager for other commands without
    having to duplicate deciding whether we're calling an external pager or
    writing directly STDOUT based on our configuration.

--- cmd/outline.go
+++ cmd/outline.go
@@ -1,13 +1,8 @@
 package cmd
 
 import (
-	"fmt"
-	"io"
 	"log"
-	"os"
-	"os/exec"
 	"strings"
-	"sync"
 
 	"github.com/msp301/zb/graph"
 	"github.com/msp301/zb/pager"
@@ -19,52 +14,29 @@ var outlineCmd = &cobra.Command{
 	Use:   "outline",
 	Short: "Output notes as a tree",
 	Run: func(cmd *cobra.Command, args []string) {
-		pager := pager.FindPager()
-
-		var pipe io.WriteCloser
-
-		var command *exec.Cmd
-		if pager == "" {
-			pipe = os.Stdout
-		} else {
-			command = exec.Command(pager)
-			command.Stdout = os.Stdout
-			command.Stderr = os.Stderr
-
-			var err error
-			pipe, err = command.StdinPipe()
-			if err != nil {
-				log.Fatal(err)
-			}
+		pager, err := pager.Open()
+		if err != nil {
+			log.Fatal(err)
 		}
 
-		var wg sync.WaitGroup
-		wg.Add(1)
-
 		go func() {
-			defer pipe.Close()
+			defer pager.Close()
 
 			book().Notes.Walk(func(vertex graph.Vertex, depth int) bool {
 				indent := strings.Repeat("\t", depth)
 				switch val := vertex.Properties["Value"].(type) {
 				case parser.Note:
-					_, err := fmt.Fprintf(pipe, "%s%s - %s\n", indent, val.File, val.Title)
+					_, err := pager.Writef("%s%s - %s\n", indent, val.File, val.Title)
 					if err != nil {
 						log.Fatal(err)
 					}
 				}
 				return true
 			}, -1)
-
-			wg.Done()
 		}()
 
-		if pager == "" {
-			wg.Wait()
-		} else {
-			if err := command.Run(); err != nil {
-				log.Fatal(err)
-			}
+		if err := pager.Wait(); err != nil {
+			log.Fatal(err)
 		}
 	},
 }
--- pager/pager.go
+++ pager/pager.go
@@ -1,7 +1,11 @@
 package pager
 
 import (
+	"fmt"
+	"io"
 	"os"
+	"os/exec"
+	"sync"
 
 	"github.com/spf13/viper"
 )
@@ -18,4 +22,51 @@ func FindPager() string {
 	}
 
 	return ""
+}
+
+type Pager struct {
+	pipe    io.WriteCloser
+	command *exec.Cmd
+	wg      sync.WaitGroup
+}
+
+func Open() (*Pager, error) {
+	pagerCmd := FindPager()
+
+	var pager = Pager{}
+	pager.wg.Add(1)
+
+	if pagerCmd == "" {
+		pager.pipe = os.Stdout
+	} else {
+		pager.command = exec.Command(pagerCmd)
+		pager.command.Stdout = os.Stdout
+		pager.command.Stderr = os.Stderr
+
+		var err error
+		pager.pipe, err = pager.command.StdinPipe()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &pager, nil
+}
+
+func (pager *Pager) Close() {
+	pager.pipe.Close()
+	pager.wg.Done()
+}
+
+func (pager *Pager) Wait() error {
+	if pager.command == nil {
+		pager.wg.Wait()
+		return nil
+	}
+
+	return pager.command.Run()
+}
+
+func (pager *Pager) Writef(format string, args ...interface{}) (int, error) {
+	return fmt.Fprintf(pager.pipe, format, args...)
 }
