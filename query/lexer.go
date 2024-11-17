481adcc4801d3701d95bd3ded0a8b0a2d8f5185b
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2024-11-17 00:22:05 +0000

    Query lexer now slurps everything
    Search query lexer does nothing other than read its input and assign
    it to a single token. Lexer is now in a state where identifying each
    token type can be implemented.

--- query/lexer.go
+++ query/lexer.go
@@ -1,5 +1,9 @@
 package query
 
+import (
+	"bytes"
+)
+
 type Lexer struct {
 	char     byte
 	input    string
@@ -14,11 +18,22 @@ func New(input string) *Lexer {
 
 func (l *Lexer) readChar() {
 	l.char = l.input[l.position]
+	l.position = l.position + 1
 }
 
 func (l *Lexer) NextToken() Token {
+	var tokenValue string
+
+	buffer := bytes.NewBufferString(tokenValue)
+	for i := 0; i < len(l.input); i++ {
+		switch l.readChar(); l.char {
+		default:
+			buffer.WriteByte(l.char)
+		}
+	}
+
 	return Token{
 		Type:  TERM,
-		Value: "Foo",
+		Value: buffer.String(),
 	}
 }
--- query/lexer_test.go
+++ query/lexer_test.go
@@ -10,16 +10,16 @@ func TestNextToken(t *testing.T) {
 		{Type: TERM, Value: "bar"},
 	}
 
-	lexer := New("bar")
+	lexer := New("bar foo")
 
 	for index, want := range tests {
 		t.Run(fmt.Sprintf("Position %d", index), func(t *testing.T) {
 			got := lexer.NextToken()
 			if got.Type != want.Type {
-				t.Fatalf("expected type %v but was %v", want.Type, got.Type)
+				t.Fatalf("expected type '%v' but was '%v'", want.Type, got.Type)
 			}
 			if got.Value != want.Value {
-				t.Fatalf("expected value %s but was %s", want.Value, got.Value)
+				t.Fatalf("expected value '%s' but was '%s'", want.Value, got.Value)
 			}
 		})
 	}
