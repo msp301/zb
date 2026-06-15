955782acd232f72caace51cb1554b664154775d6
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2026-06-15 21:59:58 +0100

    Add test around TagIntersection to highlight potential bug
    When filtering notes by related tags, check we are getting related
    notes across the supplied tags.
    
    Sometimes when getting the intersection we have matched related tags
    but no related notes (which should never happen).

--- notebook/notebook_test.go
+++ notebook/notebook_test.go
@@ -1,9 +1,10 @@
 package notebook
 
 import (
-	"github.com/msp301/graph"
 	"reflect"
 	"testing"
+
+	"github.com/msp301/graph"
 )
 
 func TestMatchedTags(t *testing.T) {
@@ -61,14 +62,45 @@ func TestSearchByTags_PrioritisesMatches(t *testing.T) {
 	g.Add(6, "tag", "#LargeLanguageModel")
 	g.Add(7, "tag", "#LargeLanguageModels")
 
-	g.Edge(3, 4, "tag")
+	g.Edge(2, 4, "tag")
 	g.Edge(3, 4, "tag")
 	g.Edge(3, 7, "tag")
 
 	book := &Notebook{Notes: g}
 
 	got := book.SearchByTags("test", "llm")
-	want := []Result{{Line: -1, Value: graph.Vertex{Id: 7, Label: "note"}}}
+	want := []Result{{Line: -1, Value: graph.Vertex{Id: 3, Label: "note"}}}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("Expected: %+v\nGot: %+v", want, got)
+	}
+}
+
+func TestTagIntersection(t *testing.T) {
+	g := graph.New()
+	g.Add(1, "note", nil)
+	g.Add(2, "note", nil)
+	g.Add(3, "note", nil)
+
+	g.Add(4, "tag", "#test")
+	g.Add(5, "tag", "#LLM")
+	g.Add(6, "tag", "#LargeLanguageModel")
+	g.Add(7, "tag", "#LargeLanguageModels")
+
+	g.Edge(2, 4, "tag")
+	g.Edge(3, 4, "tag")
+	g.Edge(3, 7, "tag")
+
+	book := &Notebook{Notes: g}
+	tags := []matchedTag{
+		{Distance: 1, Tag: "#test", Vertex: graph.Vertex{Id: 4, Label: "tag", Value: "#test"}},
+		{Distance: 1, Tag: "#LLM", Vertex: graph.Vertex{Id: 5, Label: "tag", Value: "#LLM"}},
+		{Distance: 16, Tag: "#LargeLanguageModel", Vertex: graph.Vertex{Id: 6, Label: "tag", Value: "#LargeLanguageModel"}},
+		{Distance: 17, Tag: "#LargeLanguageModels", Vertex: graph.Vertex{Id: 7, Label: "tag", Value: "#LargeLanguageModels"}},
+	}
+
+	got := book.TagIntersection(tags, 2)
+	want := []graph.Vertex{{Id: 3, Label: "note"}}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("Expected: %+v\nGot: %+v", want, got)
